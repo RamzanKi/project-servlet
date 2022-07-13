803a90f5adc267b144509d6f362e145bffd497e8
RamzanKi <91493572+RamzanKi@users.noreply.github.com>
2022-07-13 13:13:02 +0300

    Logic

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -1,5 +1,6 @@
 package com.tictactoe;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,8 +20,27 @@ public class LogicServlet extends HttpServlet {
         Field field = extractField(currentSession);
 
         int index = getSelectedIndex(req);
+        Sign currentSign = field.getField().get(index);
+
+        if (Sign.EMPTY != currentSign) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(req, resp);
+            return;
+        }
 
         field.getField().put(index, Sign.CROSS);
+        if (checkWin(resp, currentSession, field)) {
+            return;
+        }
+
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+
+        if (emptyFieldIndex >= 0) {
+            field.getField().put(emptyFieldIndex, Sign.NOUGHT);
+            if (checkWin(resp, currentSession, field)) {
+                return;
+            }
+        }
 
         List<Sign> data = field.getFieldData();
 
@@ -44,5 +64,20 @@ public class LogicServlet extends HttpServlet {
             throw new RuntimeException("Session is broke, try one more time");
         }
         return (Field) fieldAttribute;
+    }
+
+    private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
+        Sign winner = field.checkWin();
+        if (Sign.CROSS == winner || Sign.NOUGHT == winner) {
+            currentSession.setAttribute("winner", winner);
+
+            List<Sign> data = field.getFieldData();
+
+            currentSession.setAttribute("data", data);
+
+            response.sendRedirect("/index.jsp");
+            return true;
+        }
+        return false;
     }
 }
