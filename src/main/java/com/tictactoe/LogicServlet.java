d97810db0c7257d1366edb6ee6e498faec086a07
RamzanKi <91493572+RamzanKi@users.noreply.github.com>
2022-07-13 12:55:18 +0300

    Logic

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -5,20 +5,44 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 
 @WebServlet(name = "LogicServlet", value = "/logic")
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession currentSession = req.getSession();
+
+        Field field = extractField(currentSession);
+
         int index = getSelectedIndex(req);
+
+        field.getField().put(index, Sign.CROSS);
+
+        List<Sign> data = field.getFieldData();
+
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", field);
+
         resp.sendRedirect("/index.jsp");
+
     }
 
     private int getSelectedIndex(HttpServletRequest request) {
         String click = request.getParameter("click");
         boolean isNumeric = click.chars().allMatch(Character::isDigit);
         return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    private Field extractField(HttpSession currentSession) {
+        Object fieldAttribute = currentSession.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            currentSession.invalidate();
+            throw new RuntimeException("Session is broke, try one more time");
+        }
+        return (Field) fieldAttribute;
     }
 }
