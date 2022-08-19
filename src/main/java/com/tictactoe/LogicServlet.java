67a6acfddff3762574a9715055f9dd594f76ad35
RamzanKi <91493572+RamzanKi@users.noreply.github.com>
2022-08-19 12:43:35 +0300

    Logic

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -15,44 +15,62 @@ import java.util.List;
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Получаем текущую сессию
         HttpSession currentSession = req.getSession();
 
+        // Получаем объект игрового поля из сессии
         Field field = extractField(currentSession);
 
+        // получаем индекс ячейки, по которой произошел клик
         int index = getSelectedIndex(req);
         Sign currentSign = field.getField().get(index);
 
+        // Проверяем, что ячейка, по которой был клик пустая.
+        // Иначе ничего не делаем и отправляем пользователя на ту же страницу без изменений
+        // параметров в сессии
         if (Sign.EMPTY != currentSign) {
             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
             dispatcher.forward(req, resp);
             return;
         }
 
+        // ставим крестик в ячейке, по которой кликнул пользователь
         field.getField().put(index, Sign.CROSS);
+        // Проверяем, не победил ли крестик после добавление последнего клика пользователя
         if (checkWin(resp, currentSession, field)) {
             return;
         }
 
+        // Получаем пустую ячейку поля
         int emptyFieldIndex = field.getEmptyFieldIndex();
 
         if (emptyFieldIndex >= 0) {
             field.getField().put(emptyFieldIndex, Sign.NOUGHT);
+            // Проверяем, не победил ли нолик после добавление последнего нолика
             if (checkWin(resp, currentSession, field)) {
                 return;
             }
-        } else {
+        }
+        // Если пустой ячейки нет и никто не победил - значит это ничья
+        else {
+            // Добавляем в сессию флаг, который сигнализирует что произошла ничья
             currentSession.setAttribute("draw", true);
 
+            // Считаем список значков
             List<Sign> data = field.getFieldData();
 
+            // Обновляем этот список в сессии
             currentSession.setAttribute("data", data);
 
+            // Шлем редирект
             resp.sendRedirect("/index.jsp");
             return;
         }
 
+        // Считаем список значков
         List<Sign> data = field.getFieldData();
 
+        // Обновляем объект поля и список значков в сессии
         currentSession.setAttribute("data", data);
         currentSession.setAttribute("field", field);
 
@@ -75,15 +93,23 @@ public class LogicServlet extends HttpServlet {
         return (Field) fieldAttribute;
     }
 
+    /**
+     * Метод проверяет, нет ли трех крестиков/ноликов в ряд.
+     * Возвращает true/false
+     */
     private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
         Sign winner = field.checkWin();
         if (Sign.CROSS == winner || Sign.NOUGHT == winner) {
+            // Добавляем флаг, который показывает что кто-то победил
             currentSession.setAttribute("winner", winner);
 
+            // Считаем список значков
             List<Sign> data = field.getFieldData();
 
+            // Обновляем этот список в сессии
             currentSession.setAttribute("data", data);
 
+            // Шлем редирект
             response.sendRedirect("/index.jsp");
             return true;
         }
